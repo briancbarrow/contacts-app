b549299e2661bf226422f94805d7027a7d34b327
Brian Barrow <briancbarrow@gmail.com>
2016-09-03 21:02:02 -0600

    Functionality for Form submission

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
   // console.log(Object.create('Nextdoor', 'Neighbor', '123-123-1234', '123 Fake Street', 'Town', 'State'))
   var ContactsObj = {
     contacts: [
+
       {
         firstName: "Nextdoor",
         lastName: "Neighbor",
@@ -44,7 +45,7 @@ $(document).ready(function() {
     ],
     // $('.address.link').click(function(){
     //     $('.contact-header').text(this)
-    //   })
+    //   })    
   };
   var AddContact = {
     addToList: function() {
@@ -58,9 +59,36 @@ $(document).ready(function() {
             '</a></li>'
         ).data(ContactsObj.contacts[i].firstName+ContactsObj.contacts[i].lastName,ContactsObj.contacts[i]);
       };
-    }
+    },
+    addSingle: function() {
+      var i = ContactsObj.contacts.length - 1;
+      $(".address-list").append(
+            '<li class="address-list-item"><a href="#" class="address-link ' +
+            ContactsObj.contacts[i].firstName +
+            ContactsObj.contacts[i].lastName +
+            '">' + 
+            ContactsObj.contacts[i].firstName + ' ' + ContactsObj.contacts[i].lastName +
+            '</a></li>'
+        );
+    },
+    addFromSubmit: $('.contact-form').submit(function(event){
+      event.preventDefault();
+      ContactsObj.contacts.push({
+        firstName: $('#first-name').val(),
+        lastName: $('#last-name').val(),
+        phone: $('#phone').val(),
+        address: {
+          street: $('#street').val(),
+          city: $('#city').val(),
+          state: $('#state').val()
+        }
+      })
+      console.log(ContactsObj.contacts)
+      AddContact.addSingle();
+    })
   };
 
   AddContact.addToList();
-  console.log($('.NextdoorNeighbor').data('NextdoorNeighbor'));
+  // console.log($('.NextdoorNeighbor').data('NextdoorNeighbor').firstName);
+  console.log(ContactsObj.contacts)
 });
